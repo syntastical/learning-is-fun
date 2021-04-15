d5694758e96513e6648dea153f38ffb7f9dcf008
syntastic <syn@motorcityfurrycon.org>
2021-04-15 17:55:51 -0400

    Fixing scanner issue

--- src/algorithms/homework/NameSort.java
+++ src/algorithms/homework/NameSort.java
@@ -15,13 +15,24 @@ public class NameSort {
     public static void main(String[] args) {
         ArrayList<String> nameArray = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        /*
+         * Ideally you would use code like this
+         * String x = System.console().readLine();
+         *
+         * but Intellij does gracefully support this, and System.console() returns null
+         *
+         * The problem you're having is tht scanner.nextLine() grabs whatever is on the current line of input
+         * and returns immediately.  THis is another method, scanner.next() which will wait for the next "token".
+         * I made the change below.  Plase let me know if you need anything else.
+         */
 
         System.out.println("Number of names: ");
         int names = scanner.nextInt();
 
         for (int i = 0; i <= names; i++) {
             System.out.println("Name " + i + ": ");
-            String name = scanner.nextLine();
+            String name = scanner.next();
+
             nameArray.add(name);
         }
 
